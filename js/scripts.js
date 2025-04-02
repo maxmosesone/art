5a3e858418e97b2d4bb30a689a5788046d65d97a
Max Moiseev <maxmoiseevone@gmail.com>
2025-04-02 21:48:07 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,20 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error('Menu or Menu Toggle button not found!');
     }
 });
+
+// Появление футера при прокрутке до конца страницы
+window.addEventListener('scroll', () => {
+    const footer = document.querySelector('footer');
+    if (footer) {
+        const scrollPosition = window.scrollY + window.innerHeight;
+        const pageHeight = document.documentElement.scrollHeight;
+
+        if (scrollPosition >= pageHeight) {
+            footer.style.opacity = '1'; // Показываем футер
+        } else {
+            footer.style.opacity = '0'; // Скрываем футер
+        }
+    } else {
+        console.error('Footer not found!');
+    }
+});
